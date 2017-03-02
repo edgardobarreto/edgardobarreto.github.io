78599244ef1d846f053124632cd519263a4fb006
Edgardo Barreto <edgardobarretog@gmail.com>
2017-03-02 06:41:57 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,9 +1,25 @@
 <?php
-	$destino = "contacto@edgardobarreto.co";
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$message = $_POST["message"];
-	$contenido = "Nombre: ". $name . "\nCorreo: " . $email . "\nMensaje " . $message; mail($destino,"Contacto", $contenido);
-	header("Location:index.html");
-
+// Guardar los datos recibidos en variables:
+$name = $_POST['name'];
+$email = $_POST['email'];
+//$asunto = $_POST['asunto'];
+$message = $_POST['message'];
+// Definir la dirección de destino:
+$dest = "contacto@edgardobarreto.co"; 
+ 
+// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
+$headers = "From: $name $email\r\n";
+$headers .= "X-Mailer: PHP5\n";
+$headers .= 'MIME-Version: 1.0' . "\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Aqui definimos el asunto y armamos el cuerpo del mensaje
+$cuerpo = "Nombre: ".$name."<br>";
+$cuerpo .= "Email: ".$email."<br>";
+//$cuerpo .= "Asunto: ".$asunto."<br>";
+$cuerpo .= "Mensaje: ".$message;
+// Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
+if($name != '' && $email != '' &&  $message != ''){
+    mail($dest,$cuerpo,$headers); //ENVIAR!
+}
 ?>
