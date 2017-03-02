1b9456b877fad2d4cc7d7cbe71f97ab0edcc9229
Edgardo Barreto <edgardobarretog@gmail.com>
2017-03-02 06:27:17 -0500

    Create enviar.php

---
+++ enviar.php
@@ -0,0 +1,8 @@
+<?php
+	$destino = "contacto@edgardobarreto.co";
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
+	$contenido = "Nombre: ". $name . "\nCorreo: " . $email . "\nMensaje " . $message; mail($destino,"Contacto", $contenido);
+
+?>
