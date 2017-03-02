5b876f45a3de32fcae6ab20ecd4e33b6d39cdea2
Edgardo Barreto <edgardobarretog@gmail.com>
2017-03-02 06:35:13 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -4,5 +4,6 @@
 	$email = $_POST["email"];
 	$message = $_POST["message"];
 	$contenido = "Nombre: ". $name . "\nCorreo: " . $email . "\nMensaje " . $message; mail($destino,"Contacto", $contenido);
+	header("Location:index.html");
 
 ?>
